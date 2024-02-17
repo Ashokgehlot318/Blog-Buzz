1e8796ca86ffbc832f0406c1d57eb5805328b586
Ashok Gehlot <ashokgehlot318@gmail.com>
2024-02-17 17:52:11 +0530

    database is connected

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 Port = 4000;
 
 app.listen(Port, () => {
     console.log(`server is created at port no. ${Port}`)
 });
+
+mongoose.connect(process.env.DB_URL)
+    .then( ()=>{
+        console.log("MongoDB is connected successfully..");
+    })
+    .catch(error => {
+        console.log("mongoDB is facing issue...");
+        console.log(error);
+    })
