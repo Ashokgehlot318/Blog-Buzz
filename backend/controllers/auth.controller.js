35eb7662d72149d9f10177ee6040e791351a7571
Ashok Gehlot <ashokgehlot318@gmail.com>
2024-03-27 01:18:02 +0530

    Sign In API route is created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,6 +2,8 @@ const { User } = require("../models/user.model");
 const bcryptjs = require('bcryptjs');
 const { errorHandler } = require("../utils/error");
 
+const jwt = require('jsonwebtoken');
+
 exports.signup =async (req,res,next) =>{
     // console.log(req.body);
     // res.json("signup is done")
@@ -30,4 +32,42 @@ exports.signup =async (req,res,next) =>{
         // next(error);
     }
 
+}
+
+
+exports.signin = async (req,res,next) =>{
+    const { email, password } = req.body;
+
+    if(!email || !password){
+        return next(errorHandler(404,"All fields are required"));
+    }
+
+        try{
+            const validUser =await User.findOne({ email });
+            if(!validUser){
+                return next(errorHandler(404,"User not found"));
+            }
+
+            const validPassword = bcryptjs.compareSync(password,validUser.password);
+            if(!validPassword){
+               return next(errorHandler(404,'Invalid password'));
+            }
+
+            const token = jwt.sign({id: validUser._id,},process.env.JWT_SECRET,{ expiresIn:'12h' });
+
+            // first method
+            validUser.password = undefined;
+
+            // // second method 
+            // const {password:password, ...rest} = validUser._doc;
+
+            res.status(200)
+                .cookie('token', token, {
+                    httpOnly: true,
+                })
+                .json(validUser);
+        }
+        catch(error){
+            next(error);
+        }
 }
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const { signup } = require('../controllers/auth.controller');
+const { signin } = require('../controllers/auth.controller');
 
 router.post('/signup',signup);
+router.post('/signin',signin);
 
 module.exports = router;
