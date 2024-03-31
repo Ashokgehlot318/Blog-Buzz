3b1c8f8796d2e3be81290215a819b0b8cfc3c8a3
Ashok Gehlot <ashokgehlot318@gmail.com>
2024-03-31 19:55:39 +0530

    Sidebar functionality added

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashboardSidebar from '../components/DashboardSidebar';
+import DashboardProfile from '../components/DashboardProfile';
+
 
 const Dashboard = () => {
+  const location = useLocation();
+  const [tab,setTab] = useState("");
+
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if(tabFromUrl){
+      setTab(tabFromUrl);
+    }
+    setTab(tabFromUrl);
+  },[location.search]);
   return (
-    <div>
-      Dashboard
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      {/* left side-> sidebar  */}
+      <div className="md:w-56">
+          <DashboardSidebar />
+      </div>
+
+      {/* right side  */}
+
+      <div className="">
+        {
+          tab === 'profile' && <DashboardProfile />
+        }
+
+      </div>
+     
     </div>
   )
 }
