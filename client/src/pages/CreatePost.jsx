184b62dfaf540b5cc0f2448717033ec44e3905ae
Ashok Gehlot <ashokgehlot318@gmail.com>
2024-04-03 23:11:30 +0530

    Create blog post functionality added

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -16,6 +16,7 @@ const CreatePost = () => {
   const [publishError, setPublishError] = useState(null);
   const navigate = useNavigate();
 
+
   const imageUploadHandler = async() =>{
     try {
       if (!file) {
@@ -52,11 +53,37 @@ const CreatePost = () => {
       console.log(error);
     }
   }
+
+  const submitHandler = async (e)=>{
+    e.preventDefault();
+    try {
+      const res = await fetch('/api/post/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setPublishError(data.message);
+        return;
+      }
+
+      if (res.ok) {
+        setPublishError(null);
+        navigate(`/post/${data.slug}`);
+      }
+    } catch (error) {
+      setPublishError('Something went wrong');
+      console.log(error);
+    }
+  }
   return (
     <div className='p-3 max-w-3xl  mx-auto min-h-screen'>
       <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
 
-      <form className='flex flex-col gap-4'>
+      <form className='flex flex-col gap-4' onSubmit={submitHandler}>
       <div className='flex flex-col gap-4 sm:flex-row justify-between'>
           <TextInput
             type='text'
@@ -64,14 +91,14 @@ const CreatePost = () => {
             required
             id='title'
             className='flex-1'
-            // onChange={(e) =>
-            //   setFormData({ ...formData, title: e.target.value })
-            // }
+            onChange={(e) =>
+              setFormData({ ...formData, title: e.target.value })
+            }
           />
           <Select
-            // onChange={(e) =>
-            //   setFormData({ ...formData, category: e.target.value })
-            // }
+            onChange={(e) =>
+              setFormData({ ...formData, category: e.target.value })
+            }
           >
             <option value='uncategorized'>Select a category</option>
             <option value='javascript'>JavaScript</option>
@@ -116,18 +143,21 @@ const CreatePost = () => {
         )}
 
           <ReactQuill
-          theme='snow'
-          placeholder='Write something...'
-          className='h-72 mb-12'
-          required
-          // onChange={(value) => {
-          //   setFormData({ ...formData, content: value });
-          // }}
+            theme='snow'
+            placeholder='Write something...'
+            className='h-72 mb-12'
+            required
+            onChange={(value) => {setFormData({ ...formData, content: value });}}
         />
 
         <Button type='submit' gradientDuoTone='purpleToPink'>
           Publish
         </Button>
+        {publishError && (
+          <Alert className='mt-5' color='failure'>
+            {publishError}
+          </Alert>
+        )}
       </form>
 
 
