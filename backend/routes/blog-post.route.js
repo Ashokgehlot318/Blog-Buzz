83c175c014ce266794c574d9d4ad302252750abd
Ashok Gehlot <ashokgehlot318@gmail.com>
2024-04-04 19:41:05 +0530

    get posts api route is created

--- backend/routes/blog-post.route.js
+++ backend/routes/blog-post.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {verifyUser}  = require('../utils/verifyUser')
-const {create} = require('../controllers/blog-post.controller')
+const {create, getPosts} = require('../controllers/blog-post.controller')
 
 
 router.post('/create',verifyUser,create);
+router.get('/getposts',getPosts);
 
 module.exports = router;
