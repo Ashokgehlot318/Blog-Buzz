0cd43fad42b8d5c21c95025f94c6c491fb06dd6d
Ashok Gehlot <ashokgehlot318@gmail.com>
2024-04-15 00:36:25 +0530

    Reacent article comment section added

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -5,14 +5,14 @@ import PostAction from '../components/PostAction';
 import CommentSection from '../components/CommentSection';
 // import CallToAction from '../components/CallToAction';
 // import CommentSection from '../components/CommentSection';
-// import PostCard from '../components/PostCard';
+import RecentPostCard from '../components/RecentPostCard';
 
 export default function PostPage() {
   const { postslug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
-//   const [recentPosts, setRecentPosts] = useState(null);
+  const [recentPosts, setRecentPosts] = useState(null);
 console.log(postslug)
 
   useEffect(() => {
@@ -39,20 +39,20 @@ console.log(postslug)
     fetchPost();
   }, [postslug]);
 
-//   useEffect(() => {
-//     try {
-//       const fetchRecentPosts = async () => {
-//         const res = await fetch(`/api/post/getposts?limit=3`);
-//         const data = await res.json();
-//         if (res.ok) {
-//           setRecentPosts(data.posts);
-//         }
-//       };
-//       fetchRecentPosts();
-//     } catch (error) {
-//       console.log(error.message);
-//     }
-//   }, []);
+  useEffect(() => {
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      };
+      fetchRecentPosts();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
 
   if (loading)
     return (
@@ -95,13 +95,13 @@ console.log(postslug)
      
       <CommentSection postId={post._id} />
 
-      {/* <div className='flex flex-col justify-center items-center mb-5'>
+      <div className='flex flex-col justify-center items-center mb-5 '>
         <h1 className='text-xl mt-5'>Recent articles</h1>
-        <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+        <div className='flex flex-wrap gap-3 mt-5 justify-center max-w-6xl sm:flex-row'>
           {recentPosts &&
-            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+            recentPosts.map((post) => <RecentPostCard key={post._id} post={post} />)}
         </div>
-      </div> */}
+      </div>
     </main>
   );
 }
